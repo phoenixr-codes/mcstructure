17f663116d038eefc9d0e7c8a0b8d3784d962a71
EillesWan <W-YI_DoctorYI@outlook.com>
2023-03-26 19:15:23 +0800

    update sth.

--- TrimMCStruct/__init__.py
+++ TrimMCStruct/__init__.py
@@ -3,6 +3,6 @@ Read and write Minecraft .mcstructure files.
 """
 
 __version__ = "0.0.3"
-__author__ = (("金羿", "Eilles Wan"), ("诸葛亮与八卦阵", "bgArray"))
+__author__ = (("金羿", "Eilles Wan"), ("诸葛亮与八卦阵", "bgArray"), ("phoenixR","phoenixR"))
 
 from .main import *
