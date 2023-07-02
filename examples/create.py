603daacf588df42ae66b58dfa10bc095c0ce2012
phoenixr-codes <phoenixr41414141@gmail.com>
2023-07-02 18:50:14 +0200

    New project structure

--- examples/create.py
+++ examples/create.py
@@ -1,17 +1,17 @@
-from src.mcstructure import Block, Structure
+from mcstructure import Block, Structure
 
 struct = Structure(
     (6, 6, 6),  # Size of the Structure     声明结构大小，注意这是大小，其坐标从0开始
-    Block("minecraft:air")	# pre-fill blocks   预填充方块
+    Block("minecraft:air")  # pre-fill blocks   预填充方块
 )
 
 # fill blocks  填充方块
-struct.set_blocks((1,1,1),(4,1,4),Block("minecraft:iron_block"))
+struct.set_blocks((1, 1, 1), (4, 1, 4), Block("minecraft:iron_block"))
 
 # display stuffs in it  显示一些东西
 print(struct.get_structure())
 print(struct._get_str_array(with_namespace=False, with_states=True))
 
 # write into file  写入文件
-with open("example.mcstructure", "wb") as f:
+with open("structures/created_with_python.mcstructure", "wb") as f:
     struct.dump(f)
--- examples/read_and_display.py
+++ examples/read_and_display.py
@@ -1,6 +1,6 @@
 from mcstructure import Structure
 
-with open("dirt_house.mcstructure", "rb") as f:
+with open("structures/dirt_house.mcstructure", "rb") as f:
     struct = Structure.load(f)
 
 print(struct)
